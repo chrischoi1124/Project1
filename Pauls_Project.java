80746abe9aca89af8ef7f40178af46c36beb5109
Paul Choi <paulchoi0205@gmail.com>
2019-03-13 23:04:21 -0700

    Added Pauls_Project (0.1) Made a println saying hello

---
+++ Pauls_Project.java
@@ -0,0 +1,14 @@
+
+/**
+ * Paul's first test project.
+ *
+ * @author Paul Choi
+ * @version 0.1
+ */
+public class Pauls_Project
+{
+    public static void main (String [] args)
+    {
+        System.out.println("hello");
+    }
+}
